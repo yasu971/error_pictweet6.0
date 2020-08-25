edce485fdc19b13e0354f5707b92cb12b9df748b
kokkedx <kokkedx2008@gmail.com>
2020-08-25 15:22:41 +0900

    問題時

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'tweets#index'
-  resources :tweets do
-    resources :comments, only: :create
-    collection do
-      get 'search'
-    end
-  end
-  resources :users, only: :show
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    root to: 'tweets#index'
+    resources :tweets, only: [:index, :new, :create, :destroy, :edit,:update]
 end
