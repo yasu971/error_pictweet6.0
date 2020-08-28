e7e2d5a2f3326d3ce8572467c090927bb62e1595
kokkedx <kokkedx2008@gmail.com>
2020-08-28 15:03:17 +0900

    問題前

--- spec/factories/users.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-  end
-end
