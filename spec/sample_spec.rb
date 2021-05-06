bede66fb694e2c563151a7c8ca977a7ef66aba42
yasu971 <yasuishii1421@gmail.com>
2021-05-06 17:40:42 +0900

    Update sample_spec.rb

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe '簡単なテストコード' do
   describe '数字の計算' do
     it '2+3が10になるのは誤りである' do
-      expect(2 + 3 == 10).to eq false
+      # expect(2 + 3 == 10).to eq false
     end
   end
 end
